b86d6ef89bc4127a3dadc8edded0a133923cd099
Mateus <dev@DESKTOP-TQFJK1E.localdomain>
2021-05-01 14:22:10 -0300

    Adicionado feed, atualizacao do projeto

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,17 @@
+let deferredPromt
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
         .then(() => console.log('Service Worker registered!'))
 }
+if (!window.Promise) {
+    window.Promise = Promise
+}
+
+window.addEventListener('beforeinstallprompt', event => {
+    console.log('beforeinstallprompt fired')
+    event.preventDefault()
+    
+    deferredPromt = event
+    return false
+})
