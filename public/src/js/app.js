8b7d21aa84944f066da4b931412d5ba12ac3049d
Mateus André <mateusandre28@hotmail.com>
2021-05-03 12:51:29 -0300

    Adicinado imagem do feed; atualizacao do projeto

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,17 +1,20 @@
 let deferredPromt
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(() => console.log('Service Worker registered!'))
-}
 if (!window.Promise) {
     window.Promise = Promise
 }
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => console.log('Service Worker registered!'))
+        .catch(err => console.log(err))
+}
+
+
 window.addEventListener('beforeinstallprompt', event => {
     console.log('beforeinstallprompt fired')
     event.preventDefault()
-    
     deferredPromt = event
     return false
 })
